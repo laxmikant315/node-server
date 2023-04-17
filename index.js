aa7c978e71131a22623a1e5bc6d15640f95962ed
laxmikantphadke <laxmikantphadke@gmail.com>
2023-04-17 14:07:25 +0530

    asd

--- index.js
+++ index.js
@@ -21,44 +21,48 @@ let browser = null;
 let page = null;
 
 async function getResult(ticker, candelType = "day", exchange = "NSE") {
-  if (!page) {
-    console.log("Launching Browser");
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    page = await browser.newPage();
-  }
-  console.log("ticker", ticker);
-  if (ticker && ticker.toUpperCase().includes("NIFTY")) {
-    exchange = "INDICES";
-  }
-  const url = `https://mo.streak.tech/?utm_source=context-menu&utm_medium=kite&stock=${exchange}:${encodeURIComponent(
-    ticker
-  )}&theme=dark`;
-  await page.goto(url, { waitUntil: "networkidle0" });
-  // const divs = await page.$(".jss48");
-  const button = await page.evaluateHandle(
-    // () => document.querySelector(".jss47").lastChild
-    (candelType) => document.getElementById(candelType),
-    candelType
-  );
-  // const button = await page.evaluateHandle(() => {
-  //   return document.querySelector(".jss47").lastChild;
-  // });
-
-  await button.click();
-  sleep(1000);
-  // await page.waitForSelector(".jss66");
-  let results = await page.content();
-  setTimeout(async () => {
-    if (!isActive && browser && page) {
-      console.log("No Active Request, Closing Browser");
-      await browser.close();
-      browser = null;
-      page = null;
+  try {
+    if (!page) {
+      console.log("Launching Browser");
+      browser = await puppeteer.launch({
+        args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      });
+      page = await browser.newPage();
     }
-  }, [30000]);
-  return results;
+    console.log("ticker", ticker);
+    if (ticker && ticker.toUpperCase().includes("NIFTY")) {
+      exchange = "INDICES";
+    }
+    const url = `https://mo.streak.tech/?utm_source=context-menu&utm_medium=kite&stock=${exchange}:${encodeURIComponent(
+      ticker
+    )}&theme=dark`;
+    await page.goto(url, { waitUntil: "networkidle0" });
+    // const divs = await page.$(".jss48");
+    const button = await page.evaluateHandle(
+      // () => document.querySelector(".jss47").lastChild
+      (candelType) => document.getElementById(candelType),
+      candelType
+    );
+    // const button = await page.evaluateHandle(() => {
+    //   return document.querySelector(".jss47").lastChild;
+    // });
+
+    await button.click();
+    sleep(1000);
+    // await page.waitForSelector(".jss66");
+    let results = await page.content();
+    setTimeout(async () => {
+      if (!isActive && browser && page) {
+        console.log("No Active Request, Closing Browser");
+        await browser.close();
+        browser = null;
+        page = null;
+      }
+    }, [30000]);
+    return results;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 app.get("/getResult/:ticker/:candelType/:exchange?", (req, res) => {
