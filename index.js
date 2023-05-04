57cbc6b8e928919f760b388494e15ed3fb998dbc
laxmikantphadke <laxmikantphadke@gmail.com>
2023-05-04 14:29:19 +0530

    asd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const puppeteer = require("puppeteer");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 let browser = null;
 let page = null;
