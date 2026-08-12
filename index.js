e861b806cf2e35dd9310df3238d1d543939dc1a1
unknown <laxmikantphadke@gmail.com>
2026-08-12 12:49:37 +0530

    dd

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/predictStockwithAI", async (req, res) => {
   console.log("Request Body:", req.body);
 
   let { symbol, buyPrice, targetPrice, stopLoss, currentPrice } = req.body;
-  if(!buyPrice){
+  if (!buyPrice) {
     return res.status(400).json({ error: "buyPrice is required" });
   }
   let prompt = `
@@ -143,54 +143,64 @@ Make sure currentProfitLossPercent calculated correctly.
     model: req.body.model || process.env.GOOGLE_GENAI_MODEL,
     input: prompt,
   });
-  
+
 
   return res.json({ answer: JSON.parse(interaction.output_text.replace(/```json\n|```/g, '')) });
 
 })
 app.get("/getResult/:ticker/:candelType/:exchange", async (req, res) => {
-  let { ticker, candelType, exchange } = req.params;
+  try {
 
-  if (ticker && ticker.toUpperCase().includes("NIFTY")) {
-    exchange = "INDICES";
+
+    let { ticker, candelType, exchange } = req.params;
+
+    if (ticker && ticker.toUpperCase().includes("NIFTY")) {
+      exchange = "INDICES";
+    }
+    // Construct the URL with the parameters
+    const url = `https://technicalwidget.streak.tech/?utm_source=context-menu&utm_medium=kite&stock=${exchange}:${encodeURIComponent(
+      ticker
+    )}&theme=dark`
+    console.log("URL", url);
+
+    // Get a new page instance
+    const browserInstance = await getBrowserInstance();
+    const pageInstance = await browserInstance.newPage();
+
+    // Set the page timeout to 30 seconds
+    await pageInstance.setDefaultNavigationTimeout(30000);
+
+    // Navigate to the URL with a timeout of 30 seconds
+    await pageInstance.goto(url, { timeout: 30000 });
+
+    // Wait for the button with the candelType variable as the ID to appear on the page
+    if (candelType === "5min") {
+      candelType = "\\35min";
+    }
+    await pageInstance.waitForSelector(`#${candelType}`);
+
+    // Click on the button with the type variable as the ID
+    await pageInstance.click(`#${candelType}`);
+
+    // Wait for 1 second for the page to load after clicking the button
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // Get the page content and send it as a response
+    const pageContent = await pageInstance.content();
+    res.send(pageContent);
+
+    // Close the page instance
+    await pageInstance.close();
+
+    // Update the last used time for the browser
+    lastUsedTime = Date.now();
+
+  } catch (error) { 
+
+    console.log("ERROR:",{error})
+    browser = null
+    lastUsedTime = null
   }
-  // Construct the URL with the parameters
-  const url = `https://technicalwidget.streak.tech/?utm_source=context-menu&utm_medium=kite&stock=${exchange}:${encodeURIComponent(
-    ticker
-  )}&theme=dark`
-  console.log("URL", url);
-
-  // Get a new page instance
-  const browserInstance = await getBrowserInstance();
-  const pageInstance = await browserInstance.newPage();
-
-  // Set the page timeout to 30 seconds
-  await pageInstance.setDefaultNavigationTimeout(30000);
-
-  // Navigate to the URL with a timeout of 30 seconds
-  await pageInstance.goto(url, { timeout: 30000 });
-
-  // Wait for the button with the candelType variable as the ID to appear on the page
-  if (candelType === "5min") {
-    candelType = "\\35min";
-  }
-  await pageInstance.waitForSelector(`#${candelType}`);
-
-  // Click on the button with the type variable as the ID
-  await pageInstance.click(`#${candelType}`);
-
-  // Wait for 1 second for the page to load after clicking the button
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  // Get the page content and send it as a response
-  const pageContent = await pageInstance.content();
-  res.send(pageContent);
-
-  // Close the page instance
-  await pageInstance.close();
-
-  // Update the last used time for the browser
-  lastUsedTime = Date.now();
 });
 
 app.listen(port, () => {
